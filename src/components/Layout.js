fe13ea0714cd18a58f52a4a91baca0404df3eb0f
Dom Barker <dom@dombarker.co.uk>
2020-12-20 08:38:35 +0000

    seo

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,10 @@
 import React from "react"
 import { GlobalStyles } from "twin.macro"
+import SEO from "./SEO"
 
 const Layout = ({ children, ...rest }) => (
   <div {...rest}>
+    <SEO />
     <GlobalStyles />
     {children}
   </div>
